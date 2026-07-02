8faa4b998376fbe51a48a5179b32680d53092009
arashalenana <arashalenana@gmail.com>
2026-07-02 19:44:47 +0300

    Initial commit

---
+++ personal_website/pages/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home_page_view(request):
+    return HttpResponse("Homepage")
+def about_page_view(request):
+    context={"name":"Arasha", "age": 20}
+    return render(request, "pages/about.html", context)
