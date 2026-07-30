e4add8ed2d7508e401b05f8bf05b004aeb90f226
arashalenana <arashalenana@gmail.com>
2026-07-30 20:24:09 +0300

    Switch from sqlite to postgres db

--- news/django_project/settings.py
+++ news/django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from environs import env
+import dj_database_url
 env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,10 +88,10 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        env.str('DATABASE_URL'),
+        ssl_require=True
+    )
 }
 
 # Password validation
